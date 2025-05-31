7ae848177b57d14e27d0346f9e05342c3399935a
Tisha0513 <tishavarshney13@gmail.com>
2025-05-31 16:48:19 +0600

    swap

---
+++ Module 8 lesson 2/activity2.py
@@ -0,0 +1,12 @@
+x = input("Enter a number for x:")
+y = input("Enter a number for y:")
+
+print("The x value before swapping:", x)
+print("The y value before swapping:", y)
+
+temp = x
+x = y
+y = temp
+
+print("The x value after swapping:", x)
+print("The y value after swapping:", y)
