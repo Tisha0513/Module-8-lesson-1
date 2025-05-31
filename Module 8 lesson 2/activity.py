6f226a89f4aeaefc1978e205bf4a14377bbb7817
Tisha0513 <tishavarshney13@gmail.com>
2025-05-31 16:37:40 +0600

    first commit

---
+++ Module 8 lesson 2/activity.py
@@ -0,0 +1,18 @@
+name = "Tisha Varshney"
+age = 15
+isStudent = True
+Money = 225.45
+
+print("Name: ", name)
+print(type(name))
+print("Age:", age)
+print(type(age))
+print("Is Tisha a student:", isStudent)
+print(type(isStudent))
+print("Money", Money)
+print(type(Money))
+
+age = str(age)
+print("The age datatype is now ", type(age))
+money = int(Money)
+print("The money datatype is now ", type(Money))
