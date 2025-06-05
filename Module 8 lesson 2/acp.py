b1ebabf35ac309fd1dc744351f9ba73de6979f54
Tisha0513 <tishavarshney13@gmail.com>
2025-06-05 09:37:43 +0600

    swap

---
+++ Module 8 lesson 2/acp.py
@@ -0,0 +1,19 @@
+
+x = input("Enter a number for x:")
+y = input("Enter a number for y:")
+z = input("Enter a number for z:")
+
+print("The a value before swapping:" , x)
+print("The b value before swapping:" , y)
+print("The c value before swapping:" , z)
+
+temp = x
+x = y
+y = z
+z = temp
+
+
+
+print("The x value after swapping:" , x)
+print("The y value after swapping:" , y)
+print("The z value after swapping:" , z)
