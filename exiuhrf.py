7b7ae8f97545d77dbc40c46dced1da39b385715d
Tisha0513 <tishavarshney13@gmail.com>
2025-06-16 18:08:01 +0600

    exam

---
+++ exiuhrf.py
@@ -0,0 +1,18 @@
+
+total_days = int(input("Enter total number of working days: "))
+absent_days = int(input("Enter total number of days absent: "))
+
+
+attended_days = total_days - absent_days
+
+
+percentage = (attended_days / total_days) * 100
+
+
+print("Attendance percentage:", percentage)
+
+
+if percentage < 75:
+    print("You are not allowed to sit in the exam.")
+else:
+    print("You are allowed to sit in the exam.")
