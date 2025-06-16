d395dcea404c1364c68026f019b598f4b7eb1316
Tisha0513 <tishavarshney13@gmail.com>
2025-06-16 17:34:03 +0600

    *(again)

--- Module 6 lesson 4/activity2.py
+++ Module 6 lesson 4/activity2.py
@@ -1,6 +1,6 @@
 rows = int(input("Enter the number of rows: "))
 
-for t in range(2, rows + 1):
+for t in range(1, rows + 1):
     for u in range(t):
         print("*", end= " ")
-        print()
+    print()
